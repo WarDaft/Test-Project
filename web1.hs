db85b7380c98c99019c54ec3c863ff578c24a628
Steven Armstrong <wardaft@gmail.com>
2012-05-11 18:48:51 -0400

    Commit 1
    Signed-off-by: Steven Armstrong <wardaft@gmail.com>

--- web1.hs
+++ web1.hs
@@ -1,3 +1,5 @@
 import Happstack.Server
 
-main = simpleHTTP (nullConf {port = 80}) $ ok "Test."
+main = simpleHTTP (nullConf {port = 80}) $ msum [dir "main" $ path serveMain, ok "Error, unknown path."]
+
+serveMain path = ok path
