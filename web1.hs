0043693c7b7db1bfe1f9dc0dbb983ef2817787dd
Steven Armstrong <wardaft@gmail.com>
2012-05-11 22:20:34 -0400

    Signed-off-by: Steven Armstrong <wardaft@gmail.com>

--- web1.hs
+++ web1.hs
@@ -1,4 +1,7 @@
+module Main where
+
 import Control.Monad
 import Happstack.Server
 
-main = simpleHTTP (nullConf {port = 80}) $ msum [dir "main" $ ok "nar", ok "Error, unknown path."]
+
+main = simpleHTTP (nullConf {port = 80}) $ msum [dir "main" $ ok "main", ok $ "Unknown Directory."]
